d18bc2df84f8e085ae885a799b1e40951f09f915
Shamil <compshamil@gmail.com>
2025-05-23 10:48:06 -0400

    some update

---
+++ src/typesPractice.ts
@@ -0,0 +1,13 @@
+let studentsName: string = 'shamil';
+const daysOfWeek: number = 7;
+
+// Calculate rectangle area
+
+let sideWidth: number = 5;
+let sideLength: number = 10; 
+let area: number = sideLength * sideWidth;
+let modulus: number = 2
+area % modulus ? console.log(true) : console.log(false);
+console.log(area);
+
+
